5a48dae24a14bef9eb4037840126ca3633d7b346
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-09-24 21:04:10 +0300

    Add project files.

---
+++ 02. Mountain Run/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _02._Mountain_Run
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double record = double.Parse(Console.ReadLine());
+            double distance = double.Parse(Console.ReadLine());
+            double timeForOneMetres = double.Parse(Console.ReadLine());
+
+            double bonusTime = Math.Floor(distance/50);
+            double sum = distance * timeForOneMetres + bonusTime*30;
+
+            if (sum < record )
+            {
+                Console.WriteLine($"Yes! The new record is {sum:f2} seconds.");
+            }
+
+            else
+            {
+                Console.WriteLine($"No! He was {(sum-record):f2} seconds slower.");
+            }
+
+
+
+        }
+    }
+}
---
+++ 03. World Snooker Championship/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace _03._World_Snooker_Championship
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string etap = Console.ReadLine();
+            string typeTicket = Console.ReadLine();
+            int numTickets = int.Parse(Console.ReadLine());
+            char photo = char.Parse(Console.ReadLine());
+            double price = 0;
+
+            
+
+            switch (etap)
+            {
+                case "Quarter final":
+                    switch (typeTicket)
+                    {
+                        case "Standard":
+                            price = 55.5;
+                            
+                            break;
+                        case "Premium":
+                            price = 105.2;
+                            break;
+                        case "VIP":
+                            price = 118.9;
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case "Semi final":
+                    switch (typeTicket)
+                    {
+                        case "Standard":
+                            price = 75.88;
+                            break;
+                        case "Premium":
+                            price = 125.22;
+                            break;
+                        case "VIP":
+                            price = 300.4;
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case "Final":
+                    switch (typeTicket)
+                    {
+                        case "Standard":
+                            price = 110.1;
+                            break;
+                        case "Premium":
+                            price = 160.66;
+                            break;
+                        case "VIP":
+                            price = 400;
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+
+
+                default:
+                    break;
+            }
+
+            double sum = numTickets * price;
+            if (sum>4000)
+            {
+                sum = sum - sum * 0.25;
+                photo = 'N';
+            }
+            else if (sum>2500)
+            {
+                sum = sum - sum * 0.1;
+            }
+            if (photo == 'Y')
+            {
+                sum = sum + numTickets * 40;
+            }
+
+            Console.WriteLine($"{sum:f2}");
+        }
+    }
+}
